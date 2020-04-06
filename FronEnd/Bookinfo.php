5fc47ebbd795706456c8ac3b914a4e8e380bb4ad
bebo <bebo@192.168.1.7>
2020-04-06 21:10:56 +0200

    up4

--- FronEnd/Bookinfo.php
+++ FronEnd/Bookinfo.php
@@ -12,9 +12,10 @@
     <head>
         <meta charset = "UTF-8">
         <title><?php echo $ret[0]->getName()?></title>
-        <link rel = 'stylesheet' href='Supp/bookinfo.css'>
+        <link rel = 'stylesheet' href='Supp/BookInfo1.css'>
+        <link rel = 'stylesheet' href='test1.css'>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         <link rel="shortcut icon" type="image/png" href="img/mainlogo.png"/>
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/main1.css">
@@ -32,15 +33,48 @@
 
         <div class='all'>
         <div class='bookinfo'>
-            <?php echo '<img src="img/'.$ret[0]->getImage().'" alt="Shield Of Hades" width=250px height=400px>';?>
+            <?php echo '<img src="img/'.$ret[0]->getImage().'" alt="Shield Of Hades" width=280px height=400px>';?>
             <ul>
                 <li>     <b>Name:</b> <?php echo $ret[0]->getName()?></li>
                 <li>     <b>Publisher:</b> <?php echo $ret[0]->getAuthor()?></li>
-                <li>     <b>Book Condition:</b> <?php echo $ret[0]->getCondition()  ?></li>
+                                <li>     <b>Book Condition:</b> <?php echo $ret[0]->getCondition()  ?></li>
                 <li>     <b>Category:</b> <?php echo $_GET['catname']?></li>
                 <li>     <b>Quantity:</b> <?php echo $ret[0]->getStock()?></li>
                 <li>     <b>ISBN:</b> <?php echo $ret[0]->getIsbn() ?></li>
                 <li>     <b>Price:</b> <?php echo '$'.$ret[0]->getPrice()  ?></li>
+                <div class="containeruser" >
+		<div class="heading">
+		 
+                    <li class="userli" style="display: inline-block;">     <b>Seller:</b> Gerges</li>
+		  <a href="#" class="visibility-user transition is-closed"><i class="fas fa-eye"></i></a>    
+		</div>
+		
+		<div class="user transition is-closed">
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-md-4">
+						<div class="card profile-card-3">
+							<div class="background-block">
+								<img src="img/userbck.jpg" alt="profile-sample1" class="background"/>
+							</div>
+							<div class="profile-thumb-block">
+								<img src="img/defuserimg.png" alt="profile-image" class="profile"/>
+							</div>
+							<div class="card-content">
+							<h2>Abanoub Rafaat<small>Email : abanoub@yahoo.com</small></h2>
+								<span>Phone : 0123127123</span>
+							<div class="icon-block"><a href="#"><i class="fas fa-envelope"></i></a><a href="tel:+0132183213"> <i class="fas fa-phone"></i></a>
+							</div>
+							</div>
+					</div>
+				</div>
+				</div>
+				</div>
+		
+	            </div>
+
+                
             </ul>
             <span style="background-color: rgb(255, 0, 0);color: white;font-size: 25px; font-weight: bold;">Choose Quantity</span>
             <div class="col"> 
@@ -77,10 +111,10 @@
 
               <div class="text-center" style="padding-top: 20px">
                   <ul class="list-unstyled list-inline" style=" text-transform: uppercase; letter-spacing: 15px;">
-                      <i onclick="window.open('_blank')"  class="fa fa-facebook fa-2x " aria-hidden="true" style ="color:#3b5999;"></i>
-                      <i onclick="window.open('_blank')" class="fa fa-instagram fa-2x"aria-hidden="true" style ="color:#cd486b"></i>
-                      <i onclick="window.open('_blank')" class="fa fa-twitter fa-2x"aria-hidden="true" style ="color:#55acee"></i>
-                      <i onclick="window.open('_blank')" class="fa fa-youtube fa-2x"aria-hidden="true" style ="color:#cd201f" ></i>
+                      <i onclick="window.open('_blank')"  class="fab fa-facebook fa-2x " aria-hidden="true" style ="color:#3b5999;"></i>
+                      <i onclick="window.open('_blank')" class="fab fa-instagram fa-2x"aria-hidden="true" style ="color:#cd486b"></i>
+                      <i onclick="window.open('_blank')" class="fab fa-twitter fa-2x"aria-hidden="true" style ="color:#55acee"></i>
+                      <i onclick="window.open('_blank')" class="fab fa-youtube fa-2x"aria-hidden="true" style ="color:#cd201f" ></i>
   
                   </ul>
               </div>
@@ -95,5 +129,30 @@
          <script src="js/popper.min.js"></script>
        <script src="js/bootstrap.min.js"></script>
        <script src="js/cart.js"></script>
+       <script>
+           $('.visibility-user').on('click',function(){
+       
+  var $btn =  $(this);
+  var $user = $('.user');
+  console.log($btn);
+  
+  if ($btn.hasClass('is-closed')) {
+    $btn.addClass('is-open');
+    $btn.text('close')
+    $user.addClass('is-open');     
+    $user.removeClass('is-closed');
+    $btn.removeClass('is-closed');
+     
+  } else {
+    $btn.removeClass('is-open');
+    $btn.text('view')
+    $user.removeClass('is-open');     
+    $user.addClass('is-closed');
+    $btn.addClass('is-closed');
+  }
+
+                  
+});
+       </script>
     </body>
 </html>
